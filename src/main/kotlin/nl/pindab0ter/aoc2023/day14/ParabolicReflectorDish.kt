065f5150ff662e7aac001a4108a44dc73e7d876b
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2023-12-25 12:17:10 +0100

    Solve day 14 part one

--- src/main/kotlin/nl/pindab0ter/aoc2023/day14/ParabolicReflectorDish.kt
+++ src/main/kotlin/nl/pindab0ter/aoc2023/day14/ParabolicReflectorDish.kt
@@ -1,31 +1,37 @@
 package nl.pindab0ter.aoc2023.day14
 
 import nl.pindab0ter.aoc2023.day14.RockType.ROUND
+import nl.pindab0ter.aoc2023.day14.RockType.SQUARE
+import nl.pindab0ter.common.getInput
 import nl.pindab0ter.common.println
 
 fun main() {
-    val input = """
-        OOOO.#.O..
-        OO..#....#
-        OO..O##..O
-        O..#.OO...
-        ........#.
-        ..#....#.#
-        ..O..#.O.O
-        ..O.......
-        #....###..
-        #....#....
-    """.trimIndent()
+    val input = getInput(2023, 14)
 
     val rocks = parse(input)
+    val tiltedRocks = tilt(rocks)
 
-    println(calculateLoad(rocks))
+
+    println("The total load on the north support beams after tilting: ${calculateLoad(tiltedRocks)}")
 }
+
+fun tilt(rocks: Set<Rock>): Set<Rock> = rocks.fold(emptySet<Rock>()) { acc, rock ->
+    when (rock.type) {
+        SQUARE -> acc + rock
+        ROUND -> {
+            val rockAbove = acc.lastOrNull { it.x == rock.x && it.y < rock.y }
+            acc + rock.copy(y = rockAbove?.y?.plus(1) ?: 0)
+        }
+    }
+}.toSet()
 
 fun calculateLoad(rocks: Set<Rock>): Int {
     val roundRocks = rocks.filter { it.type == ROUND }.toSet()
     fun Rock.weight(): Int = rocks.maxOf(Rock::y) + 1 - y
 
+    /**
+     * The [rocks] need to be sorted by `x`, then `y`
+     */
     tailrec fun weightOf(rocks: Set<Rock>, rock: Rock, acc: Int = 0): Int {
         val remainingRocks = rocks.minus(rock)
         val rockBelow = rocks.firstOrNull { it.y == rock.y + 1 }
--- src/test/kotlin/nl/pindab0ter/aoc2023/day14/ParabolicReflectorDishKtTest.kt
+++ src/test/kotlin/nl/pindab0ter/aoc2023/day14/ParabolicReflectorDishKtTest.kt
@@ -25,4 +25,37 @@ class ParabolicReflectorDishKtTest {
 
         assertEquals(136, calculateLoad(rocks))
     }
+
+    @Test
+    fun `Move the round stones when tilting`() {
+        val input = """
+            O....#....
+            O.OO#....#
+            .....##...
+            OO.#O....O
+            .O.....O#.
+            O.#..O.#.#
+            ..O..#O..O
+            .......O..
+            #....###..
+            #OO..#....
+        """.trimIndent()
+        val expectedInput = """
+            OOOO.#.O..
+            OO..#....#
+            OO..O##..O
+            O..#.OO...
+            ........#.
+            ..#....#.#
+            ..O..#.O.O
+            ..O.......
+            #....###..
+            #....#....
+        """.trimIndent()
+
+        val rocks = parse(input)
+        val expected = parse(expectedInput)
+
+        assertEquals(expected, tilt(rocks))
+    }
 }
