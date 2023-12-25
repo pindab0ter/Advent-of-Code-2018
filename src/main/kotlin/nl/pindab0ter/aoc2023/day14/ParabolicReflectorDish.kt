61d1a6b4a43194b9dfdc976991871cbef6e9cf70
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2023-12-25 14:26:17 +0100

    Solve tests for day 14 part two, but too slow

--- src/main/kotlin/nl/pindab0ter/aoc2023/day14/ParabolicReflectorDish.kt
+++ src/main/kotlin/nl/pindab0ter/aoc2023/day14/ParabolicReflectorDish.kt
@@ -1,31 +1,81 @@
 package nl.pindab0ter.aoc2023.day14
 
+import nl.pindab0ter.aoc2023.day14.Direction.*
 import nl.pindab0ter.aoc2023.day14.RockType.ROUND
 import nl.pindab0ter.aoc2023.day14.RockType.SQUARE
 import nl.pindab0ter.common.getInput
 import nl.pindab0ter.common.println
+import nl.pindab0ter.common.timing
+import java.util.*
 
 fun main() {
     val input = getInput(2023, 14)
-
     val rocks = parse(input)
-    val tiltedRocks = tilt(rocks)
 
-
+    val tiltedRocks = tilt(rocks, EAST)
     println("The total load on the north support beams after tilting: ${calculateLoad(tiltedRocks)}")
+
+    timing {
+        // Example input with 10,000 cycles takes ±1s, so this doesn't scale.
+        val cycledRocks = (1..1_000_000_000).fold(rocks.toSortedSet(NORTH.comparator)) { acc, _ -> spinCycle(acc) }
+        println("\nThe total load on the north support beams after 1 billion cycles: ${calculateLoad(cycledRocks)}")
+    }
 }
 
-fun tilt(rocks: Set<Rock>): Set<Rock> = rocks.fold(emptySet<Rock>()) { acc, rock ->
-    when (rock.type) {
-        SQUARE -> acc + rock
-        ROUND -> {
-            val rockAbove = acc.lastOrNull { it.x == rock.x && it.y < rock.y }
-            acc + rock.copy(y = rockAbove?.y?.plus(1) ?: 0)
+enum class Direction {
+    NORTH {
+        override val comparator: Comparator<Rock> = compareBy<Rock> { it.y }.thenBy { it.x }
+    },
+    EAST {
+        override val comparator: Comparator<Rock> = compareByDescending<Rock> { it.x }.thenBy { it.y }
+    },
+    SOUTH {
+        override val comparator: Comparator<Rock> = compareByDescending<Rock> { it.y }.thenByDescending { it.x }
+    },
+    WEST {
+        override val comparator: Comparator<Rock> = compareByDescending<Rock> { it.y }.thenBy { it.x }
+    };
+
+    abstract val comparator: Comparator<Rock>
+}
+
+fun spinCycle(rocks: Set<Rock>): SortedSet<Rock> {
+    return tilt(rocks, NORTH)
+        .run { tilt(this, WEST) }
+        .run { tilt(this, SOUTH) }
+        .run { tilt(this, EAST) }
+}
+
+fun tilt(rocks: Set<Rock>, direction: Direction): SortedSet<Rock> = rocks
+    .toSortedSet(direction.comparator)
+    .fold(setOf<Rock>()) { acc, rock ->
+        val maxX = rocks.maxOf(Rock::x)
+        val maxY = rocks.maxOf(Rock::y)
+
+        when (rock.type) {
+            SQUARE -> acc + rock
+            ROUND -> {
+                val nextRock = acc.lastOrNull {
+                    when (direction) {
+                        NORTH -> it.x == rock.x && it.y < rock.y
+                        EAST -> it.x > rock.x && it.y == rock.y
+                        SOUTH -> it.x == rock.x && it.y > rock.y
+                        WEST -> it.x < rock.x && it.y == rock.y
+                    }
+                }
+
+                acc + when (direction) {
+                    NORTH -> rock.copy(y = nextRock?.y?.plus(1) ?: 0)
+                    EAST -> rock.copy(x = nextRock?.x?.minus(1) ?: maxX)
+                    SOUTH -> rock.copy(y = nextRock?.y?.minus(1) ?: maxY)
+                    WEST -> rock.copy(x = nextRock?.x?.plus(1) ?: 0)
+                }
+            }
         }
     }
-}.toSet()
+    .toSortedSet(NORTH.comparator)
 
-fun calculateLoad(rocks: Set<Rock>): Int {
+fun calculateLoad(rocks: SortedSet<Rock>): Int {
     val roundRocks = rocks.filter { it.type == ROUND }.toSet()
     fun Rock.weight(): Int = rocks.maxOf(Rock::y) + 1 - y
 
--- src/test/kotlin/nl/pindab0ter/aoc2023/day14/ParabolicReflectorDishKtTest.kt
+++ src/test/kotlin/nl/pindab0ter/aoc2023/day14/ParabolicReflectorDishKtTest.kt
@@ -1,5 +1,6 @@
 package nl.pindab0ter.aoc2023.day14
 
+import nl.pindab0ter.aoc2023.day14.Direction.NORTH
 import org.junit.jupiter.api.DisplayName
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -23,7 +24,7 @@ class ParabolicReflectorDishKtTest {
 
         val rocks = parse(input)
 
-        assertEquals(136, calculateLoad(rocks))
+        assertEquals(136, calculateLoad(rocks.toSortedSet(NORTH.comparator)))
     }
 
     @Test
@@ -56,6 +57,67 @@ class ParabolicReflectorDishKtTest {
         val rocks = parse(input)
         val expected = parse(expectedInput)
 
-        assertEquals(expected, tilt(rocks))
+        assertEquals(expected, tilt(rocks, NORTH))
+    }
+
+    @Test
+    fun `Perform a tilt cycle`() {
+        val initialState = """
+            O....#....
+            O.OO#....#
+            .....##...
+            OO.#O....O
+            .O.....O#.
+            O.#..O.#.#
+            ..O..#O..O
+            .......O..
+            #....###..
+            #OO..#....
+        """.trimIndent()
+        val afterFirstCycle = """
+            .....#....
+            ....#...O#
+            ...OO##...
+            .OO#......
+            .....OOO#.
+            .O#...O#.#
+            ....O#....
+            ......OOOO
+            #...O###..
+            #..OO#....
+        """.trimIndent()
+        val afterSecondCycle = """
+            .....#....
+            ....#...O#
+            .....##...
+            ..O#......
+            .....OOO#.
+            .O#...O#.#
+            ....O#...O
+            .......OOO
+            #..OO###..
+            #.OOO#...O
+        """.trimIndent()
+        val afterThirdCycle = """
+            .....#....
+            ....#...O#
+            .....##...
+            ..O#......
+            .....OOO#.
+            .O#...O#.#
+            ....O#...O
+            .......OOO
+            #...O###.O
+            #.OOO#...O
+        """.trimIndent()
+
+        val rocks = parse(initialState)
+        val expectedAfterFirstCycle = parse(afterFirstCycle)
+        val expectedAfterSecondCycle = parse(afterSecondCycle)
+        val expectedAfterThirdCycle = parse(afterThirdCycle)
+
+        assertEquals(expectedAfterFirstCycle, spinCycle(rocks))
+        assertEquals(expectedAfterSecondCycle, spinCycle(spinCycle(rocks)))
+        assertEquals(expectedAfterThirdCycle, spinCycle(spinCycle(spinCycle(rocks))))
     }
 }
