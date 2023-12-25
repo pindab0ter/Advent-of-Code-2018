4b183da03668e0c0d3101c5b5d77eee6c49bcfe5
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2023-12-25 18:23:37 +0100

    Solve 2015 day 1 part two

--- src/main/kotlin/nl/pindab0ter/aoc2015/day01/NotQuiteLisp.kt
+++ src/main/kotlin/nl/pindab0ter/aoc2015/day01/NotQuiteLisp.kt
@@ -6,10 +6,25 @@ import nl.pindab0ter.common.println
 fun main() {
     val input = getInput(2015, 1)
 
-    val floor = input.sumOf { character ->
-        if (character == '(') 1 as Int
-        else -1
+    val finalFloor = calculateFinalFloor(input)
+    println("The final floor is: $finalFloor")
+
+    val instructionLeadingToBasement = findInstructionLeadingToBasement(input)
+    println("\nThe location of the first instruction leading to the basement is: $instructionLeadingToBasement")
+}
+
+fun calculateFinalFloor(input: String): Int = input.sumOf { character ->
+    if (character == '(') 1 as Int
+    else -1
+}
+
+fun findInstructionLeadingToBasement(input: String): Int {
+    var floor = 0
+
+    input.forEachIndexed { index, character ->
+        if (character == '(') floor++ else floor--
+        if (floor == -1) return index + 1
     }
 
-    println(floor)
+    throw IllegalStateException("No instruction leading to the basement found")
 }
---
+++ src/test/kotlin/nl/pindab0ter/aoc2015/day01/NotQuiteLispKtTest.kt
@@ -0,0 +1,46 @@
+package nl.pindab0ter.aoc2015.day01
+
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+import kotlin.test.assertEquals
+
+@DisplayName("2015 Day 01: Not Quite Lisp")
+class NotQuiteLispKtTest {
+
+    @ParameterizedTest(name = "{1} -> {0}")
+    @MethodSource("instructionsProvider")
+    fun `Calculate the floor number`(actual: Int, input: String) {
+        assertEquals(actual, calculateFinalFloor(input))
+    }
+
+    @ParameterizedTest(name = "{1} -> {0}")
+    @MethodSource("expectedFloorProvider")
+    fun `Calculate the instruction position leading into the basement`(actual: Int, input: String) {
+        assertEquals(actual, findInstructionLeadingToBasement(input))
+    }
+
+    companion object {
+        @JvmStatic
+        fun instructionsProvider(): Stream<Arguments> = Stream.of(
+            arguments(0, "(())"),
+            arguments(0, "()()"),
+            arguments(3, "((("),
+            arguments(3, "(()(()("),
+            arguments(3, "))((((("),
+            arguments(-1, "())"),
+            arguments(-1, "))("),
+            arguments(-3, ")))"),
+            arguments(-3, ")())())"),
+        )
+
+        @JvmStatic
+        fun expectedFloorProvider(): Stream<Arguments> = Stream.of(
+            arguments(1, ")"),
+            arguments(5, "()())"),
+        )
+    }
+}
