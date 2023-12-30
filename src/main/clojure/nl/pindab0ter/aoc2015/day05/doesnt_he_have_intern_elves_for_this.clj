d909455c846954beca7a951044e48e4f1be59add
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2023-12-31 00:21:50 +0100

    Solve 2015 day 5 part two with Clojure

--- src/main/clojure/nl/pindab0ter/aoc2015/day05/doesnt_he_have_intern_elves_for_this.clj
+++ src/main/clojure/nl/pindab0ter/aoc2015/day05/doesnt_he_have_intern_elves_for_this.clj
@@ -2,7 +2,9 @@
   (:require [clojure.string :as str]
             [nl.pindab0ter.common.advent-of-code :refer [get-input]]))
 
-(defn nice? [s]
+;; Challenging myself to do this without regex
+
+(defn old-nice? [s]
   (let [vowels          #{\a \e \i \o \u}
         forbidden       #{"ab" "cd" "pq" "xy"}
         three-vowels?   (>= (count (filter vowels s)) 3)
@@ -10,6 +12,30 @@
         no-forbidden?   (not (some #(str/includes? s %) forbidden))]
     (and three-vowels? double-letters? no-forbidden?)))
 
+(defn first-pair-non-unique?
+  "Checks if the first pair of the collection is non-unique."
+  [coll]
+  (let [a (first coll)]
+    (some #(= a (nth coll %)) (range 2 (count coll)))))
+
+(defn contains-two-pairs?
+  "Checks if the string contains at least two pairs of letters that are non-unique."
+  [s]
+  (let [indices (range 0 (dec (count s)))
+        pairs   (map #(take 2 (drop % s)) indices)]
+    (boolean (some #(first-pair-non-unique? (drop % pairs)) indices))))
+
+(defn contains-split-pair?
+  "Checks if the string contains a pair of letters that is separated by one letter."
+  [s]
+  (let [indices (range 0 (- (count s) 2))
+        triples (map #(take 3 (drop % s)) indices)]
+    (boolean (some #(= (first %) (last %)) triples))))
+
+(defn new-nice? [s]
+  (and (contains-two-pairs? s) (contains-split-pair? s)))
+
 (defn -main []
   (let [lines (str/split-lines (get-input 2015 5))]
-    (println "Amount of strings that are nice:" (count (filter nice? lines)))))
+    (println "Amount of strings that are nice by the old rules:" (count (filter old-nice? lines)))
+    (println "Amount of strings that are nice by the new rules:" (count (filter new-nice? lines)))))
--- src/test/clojure/nl/pindab0ter/aoc2015/day05/doesnt_he_have_intern_elves_for_this_test.clj
+++ src/test/clojure/nl/pindab0ter/aoc2015/day05/doesnt_he_have_intern_elves_for_this_test.clj
@@ -1,12 +1,20 @@
 (ns nl.pindab0ter.aoc2015.day05.doesnt-he-have-intern-elves-for-this-test
   (:require [clojure.test :refer :all])
-  (:require [nl.pindab0ter.aoc2015.day05.doesnt-he-have-intern-elves-for-this :refer [nice?]]))
+  (:require [nl.pindab0ter.aoc2015.day05.doesnt-he-have-intern-elves-for-this :refer [old-nice? new-nice?]]))
 
-(deftest nice?-test
+(deftest old-nice?-test
   (are [expected input]
-    (= expected (nice? input))
+    (= expected (old-nice? input))
     true "ugknbfddgicrmopn"
     true "aaa"
     false "jchzalrnumimnmhp"
     false "haegwjzuvuyypxyu"
     false "dvszwmarrgswjxmb"))
+
+(deftest new-nice?-test
+  (are [expected input]
+    (= expected (new-nice? input))
+    true "qjhvhtzxzqqjkmpb"
+    true "xxyxx"
+    false "uurcxstgmygtbstg"
+    false "ieodomkazucvgmuy"))
