2a4df4dbfc7fe9ec2ae47560f1ead23af019895f
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2024-01-14 10:46:06 +0100

    Improve formatting

--- src/main/clojure/nl/pindab0ter/aoc2015/day10/elves_look_elves_say.clj
+++ src/main/clojure/nl/pindab0ter/aoc2015/day10/elves_look_elves_say.clj
@@ -4,20 +4,22 @@
 
 
 (defn look-and-say [sequence]
-   (loop [remaining-sequence (rest sequence)
-          count              1
-          number             (first sequence)
-          new-sequence       []]
-     (if (empty? remaining-sequence)
-       (conj new-sequence count number)
-       (let [element (first remaining-sequence)]
-         (if (= element number)
-           (recur (rest remaining-sequence) (inc count) number new-sequence)
-           (recur (rest remaining-sequence) 1 element (conj new-sequence count number)))))))
+  (loop [remaining-sequence (rest sequence)
+         count              1
+         number             (first sequence)
+         new-sequence       []]
+    (if (empty? remaining-sequence)
+      (conj new-sequence count number)
+      (let [element (first remaining-sequence)]
+        (if (= element number)
+          (recur (rest remaining-sequence) (inc count) number new-sequence)
+          (recur (rest remaining-sequence) 1 element (conj new-sequence count number)))))))
 
 
 (defn -main []
   (let [input       (get-input 2015 10)
         sequence-in (map #(Integer/parseInt (str %)) input)]
-    (println "The length of the sequence after 40 iterations is:" (time (count (nth (iterate look-and-say sequence-in) 40))) \newline)
-    (println "The length of the sequence after 50 iterations is:" (time (count (nth (iterate look-and-say sequence-in) 50))))))
+    (println "The length of the sequence after 40 iterations is:"
+             (time (count (nth (iterate look-and-say sequence-in) 40))) \newline)
+    (println "The length of the sequence after 50 iterations is:"
+             (time (count (nth (iterate look-and-say sequence-in) 50))))))
