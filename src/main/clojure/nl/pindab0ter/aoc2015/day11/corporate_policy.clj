04bd88960e21c7be1e34c9c8181978b31fc46c84
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2024-01-15 12:57:14 +0100

    Solve 2015 day 11 part one with Clojure

--- src/main/clojure/nl/pindab0ter/aoc2015/day11/corporate_policy.clj
+++ src/main/clojure/nl/pindab0ter/aoc2015/day11/corporate_policy.clj
@@ -1,23 +1,25 @@
-(ns nl.pindab0ter.aoc2015.day11.corporate-policy)
+(ns nl.pindab0ter.aoc2015.day11.corporate-policy
+  (:require
+    [nl.pindab0ter.common.advent-of-code :refer [get-input]]))
 
 
-(defn- consecutive? [s]
+(defn consecutive? [s]
   (every? (fn [[c1 c2]] (= (inc (int c1)) (int c2)))
           (partition 2 1 (map char s))))
 
 
-(defn- three-consecutive-letters? [s]
+(defn three-consecutive-letters? [s]
   (->> s (partition 3 1)
        (some consecutive?)
        (boolean)))
 
 
-(defn- no-disallowed-characters? [s]
+(defn no-disallowed-characters? [s]
   (not (re-find #"[iol]" s)))
 
 
-(defn- contains-two-pairs? [s]
-  (>= (->> s (map char) (partition 2 1) (filter (fn [[a b]] (= a b))) count) 2))
+(defn contains-two-pairs? [s]
+  (boolean (re-find #"(?:(?:([a-z])\1).*?){2}" s)))
 
 
 (defn valid-password? [s]
@@ -27,6 +29,34 @@
     (contains-two-pairs? s)))
 
 
+(defn increment-char [^Character c]
+  (-> c
+      (int)
+      (- (int \a) -1)
+      (mod 26)
+      (+ (int \a))
+      (char)))
+
+
+(defn increment [s]
+  (->> s
+       reverse
+       rest
+       (reduce
+         (fn [acc c]
+           (conj acc (if (every? #(= \a %) acc) (increment-char c) c)))
+         [(->> s reverse first increment-char)])
+       reverse
+       (apply str)))
+
+
+(defn next-password [s]
+  (loop [candidate s]
+    (if (valid-password? candidate)
+      candidate
+      (recur (increment candidate)))))
+
+
 (defn -main []
-  (let [input "abbcegjk"]
-    (println (valid-password? input))))
+  (let [input (get-input 2015 11)]
+    (println (next-password input))))
--- src/test/clojure/nl/pindab0ter/aoc2015/day11/corporate_policy_test.clj
+++ src/test/clojure/nl/pindab0ter/aoc2015/day11/corporate_policy_test.clj
@@ -1,7 +1,7 @@
 (ns nl.pindab0ter.aoc2015.day11.corporate-policy-test
   (:require
     [clojure.test :refer :all]
-    [nl.pindab0ter.aoc2015.day11.corporate-policy :refer [valid-password?]]))
+    [nl.pindab0ter.aoc2015.day11.corporate-policy :refer [next-password valid-password?]]))
 
 
 (deftest valid-password?-test
@@ -12,3 +12,10 @@
     "abbcegjk" false
     "abcdffaa" true
     "ghjaabcc" true))
+
+
+(deftest next-password-test
+  (are [input expected]
+    (= (next-password input) expected)
+    "abcdefgh" "abcdffaa"
+    "ghijklmn" "ghjaabcc")) ;; Test takes ±16s
